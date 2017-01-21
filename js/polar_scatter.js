41c7ec0502a193d62bef9c17eca4e8559172caa0
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-01-21 22:38:32 +0000

    added test points

--- js/polar_scatter.js
+++ js/polar_scatter.js
@@ -11,6 +11,8 @@ var centerX = 300;
 var centerY = 300;
 var radius = 300;
 
+var points = [[100,30],[100,40]]
+
 for (var angle = 0; angle < 360; angle+=30){
 
 	var theta = (angle * Math.PI)/180;
@@ -24,3 +26,13 @@ for (var angle = 0; angle < 360; angle+=30){
 		.style("stroke-width","1");
 
 }
+
+points.forEach(function(point){
+
+	svg.append("circle")
+		.attr("cx", centerX + point[0] * Math.cos(point[1]))
+		.attr("cy", centerY + point[0] * Math.sin(point[1]))
+		.attr("r", 2)
+		.style("stroke","blue");
+
+});
