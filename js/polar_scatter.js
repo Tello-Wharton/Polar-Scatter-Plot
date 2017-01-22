d26a3fda527493b135aa7577113127ab6fdf947a
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-01-22 01:26:47 +0000

    Now graphing multiple examples of points

--- js/polar_scatter.js
+++ js/polar_scatter.js
@@ -44,7 +44,7 @@ for (var angle = 0; angle < 360; angle+=30){
 		.attr("y1", centerY)
 		.attr("x2", centerX + radius * Math.cos(theta))
 		.attr("y2", centerY + radius * Math.sin(theta))
-		.style("stroke","black")
+		.style("stroke","#535154")
 		.style("stroke-width","1");
 
 	svg.append("text")
@@ -52,16 +52,24 @@ for (var angle = 0; angle < 360; angle+=30){
 		.attr("x",20)
 		.attr("y",20)
 		.attr("transform", "translate(700,370) rotate(-" + angle + " -300 30)")
-		.attr('fill', '#000');
+		.attr('fill', '#535154');
 
 }
 
 points.forEach(function(point){
 
 	svg.append("circle")
-		.attr("cx", centerX + point[0] * Math.cos(point[1]))
-		.attr("cy", centerY + point[0] * Math.sin(point[1]))
-		.attr("r", 2)
-		.style("stroke","blue");
+		.attr("cx", centerX + point[0] * Math.cos(point[1] * Math.PI/180))
+		.attr("cy", centerY + point[0] * Math.sin(point[1] * Math.PI/180))
+		.attr("r", 3)
+		.style("stroke","#396AB1")
+		.style("fill","#396AB1");
 
 });
+
+svg.append("circle")
+		.attr("cx", centerX + 300 * Math.cos(202.5 * Math.PI/180))
+		.attr("cy", centerY + 300 * Math.sin(202.5 * Math.PI/180))
+		.attr("r", 3)
+		.style("stroke","#CC2529")
+		.style("fill","#CC2529");
